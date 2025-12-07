c370f390cd955ee92ee95413f085460c9c8d5cf1
Copter-239 <scherbinasu@gmail.com>
2025-12-07 18:48:19 +0300

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,8 +1,8 @@
 import os
 open('/home/clover/catkin_ws/src/clover/clover/launch/aruco.launch','w').write(open('launch/aruco.launch','r').read())
 open('/home/clover/catkin_ws/src/clover/clover/launch/clover.launch','w').write(open('launch/clover.launch','r').read())
-os.system('mv data/model/* /home/clover/catkin_ws/src/clover/clover_simulation/models/')
 os.system('pip install --upgrade flask')
+
 
 
 
