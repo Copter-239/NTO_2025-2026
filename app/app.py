3d969b17b79365e2150f477f42614f96dc92b670
Copter-239 <scherbinasu@gmail.com>
2025-12-07 16:50:01 +0300

    Add files via upload

--- app/app.py
+++ app/app.py
@@ -1,28 +1,28 @@
-# import rospy
-# from clover import srv, long_callback
-# from mavros_msgs.srv import CommandBool
-# from std_msgs.msg import UInt16MultiArray, Bool
-# from std_srvs.srv import Trigger
+import rospy
+from clover import srv, long_callback
+from mavros_msgs.srv import CommandBool
+from std_msgs.msg import UInt16MultiArray, Bool
+from std_srvs.srv import Trigger
 from flask import Flask, request, render_template
 import base64, cv2
 from threading import Thread
-# arming = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
-# land_dron = rospy.ServiceProxy('land', Trigger)
-# navigate = rospy.ServiceProxy('navigate', srv.Navigate)
-# start_bool = rospy.Publisher('bool_start', Bool, queue_size=1)
-# rospy.init_node('server_web_ui')
+arming = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
+land_dron = rospy.ServiceProxy('land', Trigger)
+navigate = rospy.ServiceProxy('navigate', srv.Navigate)
+start_bool = rospy.Publisher('bool_start', Bool, queue_size=1)
+rospy.init_node('server_web_ui')
 app = Flask(__name__)
 def msg_convert(data, obj):
     msg = obj()
     msg.data = data
     return msg
-# from cv_bridge import CvBridge
-# from sensor_msgs.msg import Image
-# bridge = CvBridge()
-# @long_callback
-# def image_callback(data):
-#     global img_now_down_cam
-#     img_now_down_cam = bridge.imgmsg_to_cv2(data, 'bgr8')\
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+bridge = CvBridge()
+@long_callback
+def image_callback(data):
+    global img_now_down_cam
+    img_now_down_cam = bridge.imgmsg_to_cv2(data, 'bgr8')
 cap = cv2.VideoCapture(0)
 def get_data_img_cam_PC():
     global img_now_down_cam
@@ -56,12 +56,12 @@ def kill():
     # start_bool.publish(msg_convert(False, Bool))
     # arming(False)
     return 'ok', 200
-@app.route('/img_down_cam')
-def img_down_cam():
-    return img_to_base64_rgb(img_now_down_cam)
-# rospy.Subscriber('main_camera/image_raw', Image, image_callback)
-f = Thread(target=get_data_img_cam_PC)
-f.start()
+# @app.route('/img_down_cam')
+# def img_down_cam():
+#     return img_to_base64_rgb(img_now_down_cam)
+rospy.Subscriber('main_camera/image_raw', Image, image_callback)
+# f = Thread(target=get_data_img_cam_PC)
+# f.start()
 
 
 if __name__ == '__main__':
