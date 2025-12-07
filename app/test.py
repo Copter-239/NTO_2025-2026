126cb0d1e089262fbd917f3797b084f3a64f5673
Copter-239 <scherbinasu@gmail.com>
2025-12-08 02:04:38 +0300

    Update test.py

--- app/test.py
+++ app/test.py
@@ -1,8 +1,11 @@
 from subprocess import run
 from multiprocessing import Process
+
 def start_flight():
     return run(["python3", r"../flight.py"])
 
 
 process_running_flight = Process(target=start_flight)
 process_running_flight.start()
+print(__file__)
+print(__name__)
