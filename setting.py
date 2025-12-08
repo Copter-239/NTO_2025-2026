9fe18f29d9a9bd09a0074206073f124fe5b66dca
Copter-239 <scherbinasu@gmail.com>
2025-12-08 13:44:33 +0300

    Add files via upload

--- setting.py
+++ setting.py
@@ -1,8 +1,8 @@
 import os
 open('/home/clover/catkin_ws/src/clover/clover/launch/aruco.launch','w').write(open('launch/aruco.launch','r').read())
 open('/home/clover/catkin_ws/src/clover/clover/launch/clover.launch','w').write(open('launch/clover.launch','r').read())
+open('/home/clover/catkin_ws/src/clover/clover_simulation/models/camera/camera.sdf','w').write(open('camera.sdf','r').read())
 os.system('pip install --upgrade flask')
-os.system('pip install --upgrade opencv-python')
 
 
 
