ab072fd2cd24e25c3ee5d387a26c5d3799ca9086
Copter-239 <scherbinasu@gmail.com>
2025-12-08 12:45:02 +0300

    Update setting.py

--- setting.py
+++ setting.py
@@ -2,6 +2,7 @@ import os
 open('/home/clover/catkin_ws/src/clover/clover/launch/aruco.launch','w').write(open('launch/aruco.launch','r').read())
 open('/home/clover/catkin_ws/src/clover/clover/launch/clover.launch','w').write(open('launch/clover.launch','r').read())
 os.system('pip install --upgrade flask')
+os.system('pip install --upgrade opencv-python')
 
 
 
